14692141555e756b0b296ab23d34adc1ba0886e8
Paul TC Huang <paul891112@hotmail.com>
2023-10-31 09:56:28 +0100

    Add array implementation, modification to envs_set and envs_get, add one example in example_operations.gsc

---
+++ example_operations.gsc
@@ -0,0 +1,7 @@
+[
+    "abfolge",
+    ["array", "new", "array1", 2],
+    ["array", "set", "array1", 0, 1],
+    ["array", "set", "array1", 1, 65],
+    ["array", "get", "array1", 1]
+]
--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -3,12 +3,14 @@ import json
 
 #---from funcs-demo in session 4---
 def do_funktion(envs,args):
+    #define function
     assert len(args) == 2
     params = args[0]
     body = args[1]
     return ["funktion",params,body]
 
 def do_aufrufen(envs,args):
+    #call function
     assert len(args) >= 1
     name = args[0]
     arguments = args[1:]
@@ -19,37 +21,41 @@ def do_aufrufen(envs,args):
     assert isinstance(func,list)
     assert func[0] == "funktion"
     func_params = func[1]
-    assert len(func_params) == len(values)
+    assert len(func_params) == len(values) #assert values passed to func is same as values required by definition
 
-    local_frame = dict(zip(func_params,values))
-    envs.append(local_frame)
-    body = func[2]
+    local_frame = dict(zip(func_params,values)) # dict of param name and values
+    envs.append(local_frame) #push this local frame/ dict to top of envs
+    body = func[2] 
     result = do(envs,body)
     envs.pop()
 
     return result
 
-def envs_get(envs, name):
+def envs_get(envs, name, index = None):
+    #get defined function from the top most possible frame
     assert isinstance(name,str)
     for e in reversed(envs):
         if name in e:
-            return e[name]
+            if index == None:
+                return e[name]
+            else:
+                return e[name][index]
         
-    # python like version
-    # if name in envs[-1]:
-    #    return e[name]
-    #if name in envs[0]:
-    #    return e[name]
     assert False, f"Unknown variable name {name}"
 
-def envs_set(envs,name,value):
+def envs_set(envs,name,value, i = None):
+    #save variables and functions to 
+    #paul adds the possibility to add lists and dictionaries
+    #variables have name as key and default data type value as value
+    #functions have name as key and a list ["Funktion", "params", "body"] as value
+    #arrays have name as key and a list ["array", size, list of fixed size ] as value
+
     assert isinstance(name,str)
-    # for e in reversed(envs):
-    #     if name in e:
-    #         e[name] = value
-    #         return
-    envs[-1][name] = value
-        
+
+    if i==None:
+        envs[-1][name] = value #in the most local frame, assign value to key "name"
+    else:
+        envs[-1][name][i] = value
 
 
 def do_setzen(envs,args):
@@ -88,21 +94,6 @@ def do_abfolge(envs,args):
     return result
 
 
-OPERATIONS = {
-    func_name.replace("do_",""): func_body
-    for (func_name, func_body) in globals().items()
-    if func_name.startswith("do_")
-}
-
-
-def do(envs,expr):
-    if isinstance(expr,int):
-        return expr
-   
-    assert isinstance(expr,list)
-    assert expr[0] in OPERATIONS, f"Unknown operation {expr[0]}"
-    func = OPERATIONS[expr[0]]
-    return func(envs, expr[1:])
 
 
 #--- end of funcs-demo in session 4---
@@ -123,22 +114,94 @@ def do_print():
 
 def do_while():
     #Exercise in book, book says use python while or recursion
+    #==, !=, <, >
     pass
 
-def do_array():
+def do_array(envs, args):
     """
     Array operations: create new array of fixed size, get value at index i, set value at index i
+    Arrays have name as key and a list ["array", size, list of fixed size ] as value in e in envs
     
     Args:
-        
+        args: list of arguments - args[0] = "array", args[1] = operation, args[2] = name, args[3] = value
+        envs: environment that keeps track of all variables
+
     
     Returns:
-        arr: the array if a new array is created
-        val: the value at index i
-        None: set new value at index i
+        new: returns a list ["array", size, list of fixed size]
+        get: returns value at index i of array
+        set: returns value set to index i of array
+        
     """
 
-    pass
+    def array_new(envs, args):
+        """
+        Creates array called name of size n
+        equivalent to do_funktion and setzen
+
+        Args: 
+            envs: environment
+            args: [name: str, n: int]
+        Return:
+            None
+        """
+        assert len(args) == 2
+        assert isinstance(args[0], str) and isinstance(args[1], int)
+
+        res = [None]*args[1]
+        envs_set(envs, args[0], res)
+        return ["array", args[1], res]
+
+    
+    def array_get(envs, args):
+        """
+        get the value of array at certain index
+        equivalent to do_abrufen
+
+        Args: 
+            envs: environment
+            args: [name: str, i: int]
+        Return:
+            int - value at name[i]
+        """
+        assert len(args) == 2
+        assert isinstance(args[0], str) and isinstance(args[1], int)
+        return envs_get(envs, args[0], args[1])
+
+    def array_set(envs, args):
+        """
+        set the value of array at certain index
+        Equivalent to do_setzen
+
+        Args: 
+            envs: environment
+            args: [name: str, i: int, val: value]
+        Return:
+            int - value at name[i]
+        """
+        assert len(args) == 3
+        assert isinstance(args[0], str) and isinstance(args[1], int)
+        array_name = args[0]
+        i = args[1]
+        value = do(envs,args[2])        
+        envs_set(envs, array_name, value, i) #check index passed to envs_set
+        return value
+
+
+    #introspection in do_array()
+    d = locals().copy()
+    OPERATIONS_ARRAY = {}
+    for k in d:
+        if k.startswith("array_"):
+            OPERATIONS_ARRAY[k.replace("array_","")] = d[k]
+
+   
+    assert args[0] in OPERATIONS_ARRAY, f"Unknown operation {args[0]}"
+    func = OPERATIONS_ARRAY[args[0]]
+    return func(envs, args[1:])
+
+
+
 
 def do_dictionary():
     pass
@@ -148,7 +211,25 @@ def do_dictionary():
 #---------end of 1, put over do() when done?-----
 
 
+#-----OPERATIONS and do() from funcs-demo.py in session 4------------
 
+OPERATIONS = {
+    func_name.replace("do_",""): func_body
+    for (func_name, func_body) in globals().items()
+    if func_name.startswith("do_")
+}
+
+
+def do(envs,expr):
+    if isinstance(expr,int):
+        return expr
+   
+    assert isinstance(expr,list)
+    assert expr[0] in OPERATIONS, f"Unknown operation {expr[0]}"
+    func = OPERATIONS[expr[0]]
+    return func(envs, expr[1:])
+
+#----end OPERATIONS and do() -----------------------
 
 #--------- 2 Object System ------------
 
@@ -206,8 +287,11 @@ def main_in_funcs_demo():
     print(f"=> {result}")
 
 def main():
-    legal_input()
+    #legal_input()
     #continue implementation
+    main_in_funcs_demo()
+
+
 
 if __name__ == "__main__":
     main()
